1437ba7afe58d16a6b187b2a830d661db9ab57b8
RehanaMulla33 <89864443+RehanaMulla33@users.noreply.github.com>
2021-09-02 20:27:08 +0530

    Add files via upload

---
+++ PrePostInc.java
@@ -0,0 +1,20 @@
+class PrePostInc{
+	public static void main(String args[]){
+		int a=10;
+		int b=11;
+		int c=12;
+		
+		
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		System.out.println("c="+c);
+		System.out.println("After pre increment addion of a, b, c are");
+		System.out.println(++a+ ++b + ++c);
+		System.out.println("After post inrement addion of a, b, c are");
+		System.out.println(a++ + b++ +c++);
+		System.out.println("after post increment the values of a b c are");
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		}
+		}
---
+++ TypeCast.java
@@ -0,0 +1,16 @@
+class TypeCast{
+	public static void main(String args[]){
+		byte a=10;
+		short b=a;
+		int c=567;
+		int c=b;
+		float d=b;
+		double e=d;
+		System.out.println("Byte value of a is="+a);
+		System.out.println(" short value b is="+b);
+		System.out.println("int value of c is="+c);
+		System.out.println("float value of d is="+d);
+		System.out.println("double value of e is="+e);
+		 
+		}
+		}
