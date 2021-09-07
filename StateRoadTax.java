730dccbb16fd909c52ffcfc0e2a8fb9da900b95c
RehanaMulla33 <89864443+RehanaMulla33@users.noreply.github.com>
2021-09-07 13:02:43 +0530

    Add files via upload

---
+++ StateRoadTax.java
@@ -0,0 +1,23 @@
+package com.xworkz.controlstatements;
+
+public class StateRoadTax {
+
+	public static void main(String[] args) {
+		String state = "KA";
+
+		if (state == "KA") {
+			System.out.println("The road tax of karnataka is 200 Rs");
+		} else if (state == "TS") {
+			System.out.println("The road tax of Telangana is 500 Rs");
+		} else if (state == "MH") {
+			System.out.println("The road tax of Maharashtra is 400 Rs");
+		} else if (state == "TN") {
+			System.out.println("The road tax of tamilnadu is 600 Rs");
+		} else if (state == "UP") {
+			System.out.println("The road tax of Uttar pradesh is 800 Rs");
+
+		}
+
+	}
+
+}
